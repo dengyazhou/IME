1df7d79a143805744e1f4ea177f0760a33fd755f
dengyazhou <1283536241@qq.com>
2020-05-14 10:26:30 +0800

    添加DemoTestGit项目测试一下

---
+++ DemoTestGit/DemoTestGit/SceneDelegate.h
@@ -0,0 +1,16 @@
+//
+//  SceneDelegate.h
+//  DemoTestGit
+//
+//  Created by 邓亚洲 on 2020/5/14.
+//  Copyright © 2020 DYZ. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SceneDelegate : UIResponder <UIWindowSceneDelegate>
+
+@property (strong, nonatomic) UIWindow * window;
+
+@end
+
---
+++ DemoTestGit/DemoTestGit/ViewController.m
@@ -0,0 +1,24 @@
+//
+//  ViewController.m
+//  DemoTestGit
+//
+//  Created by 邓亚洲 on 2020/5/14.
+//  Copyright © 2020 DYZ. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+    NSLog(@"123");
+}
+
+
+@end
